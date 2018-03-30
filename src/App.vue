f554cb27b665b9e236d6171968793bb929412b83
Stan Smith <choicedesign@gmail.com>
2018-03-29 23:55:53 -0600

    Add Contentful link to the footer

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,12 @@
     <router-view/>
 
     <footer class="footer">
-      <div>
+      <div class="footer--item">
         <!-- TODO: Use SvgIcon component -->
         Made with <svg-icon icon-name="yin-yang"></svg-icon> by <a href="https://twitter.com/stanosmith">@stanosmith</a>
+      </div>
+      <div class="footer--item">
+        <a href="https://www.contentful.com/" rel="nofollow" target=“_blank”><img src="https://images.contentful.com/fo9twyrwpveg/7Htleo27dKYua8gio8UEUy/0797152a2d2f8e41db49ecbf1ccffdaa/PoweredByContentful_DarkBackground_MonochromeLogo.svg" style="max-width:100px;width:100%;" alt="Powered by Contentful" /></a>
       </div>
     </footer>
   </div>
