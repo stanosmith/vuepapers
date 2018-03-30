51e6e236fd7959750177a00aca5703ae3e84ae32
Stan Smith <choicedesign@gmail.com>
2018-03-30 00:26:28 -0600

    Add some wallpapers - resolve #3

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,29 +2,64 @@
   <div>
     <div class="hero">
       <div class="hero--content">
-        <img class="logo img-fluid" src="../../static/vuepapers-mark.svg">
+        <img class="logo img-fluid"
+             src="../../static/vuepapers-mark.svg">
         <h1>{{ msg }}</h1>
         <p>
-          <code>Vue.js themed wallpapers for nerds. Coming soon&hellip;</code>
+          <code>Vue.js themed wallpapers for nerds.</code>
         </p>
       </div>
     </div>
-    <!--<section>
-      <picture>
-        <source srcset="">
-        <img src=""
-             alt=""></picture>
-    </section>-->
+    <section class="section cards"
+             v-if="allWallpapers.items">
+      <div class="card"
+         v-for="wallpaper in allWallpapers.items"
+         :key="wallpaper.sys.id">
+        <img :src="`https://res.cloudinary.com/stanosmith/image/upload/w_200${wallpaper.fields.cloudinaryUrl}`"
+             alt="">
+        <h3>{{wallpaper.fields.name}}</h3>
+        <div class="card--controls">
+          <!-- TODO: Convert to button and force a download instead of opening a new tab -->
+          <a :href="`https://res.cloudinary.com/stanosmith/image/upload${wallpaper.fields.cloudinaryUrl}`"
+             :title="`Download ${wallpaper.fields.name}`"
+             target="wallpaper">
+            <f-icon icon-name="download"></f-icon>
+          </a>
+          <!-- TODO: Add attribution info -->
+          <!-- TODO: Add author info -->
+          <!--<button class="btn"
+                  type="button">
+            <f-icon icon-name="heart"></f-icon>
+          </button>
+          <button class="btn"
+                  type="button">
+            <f-icon icon-name="info"></f-icon>
+          </button>-->
+          <!--<pre><code>{{ wallpaper }}</code></pre>-->
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex';
+import fIcon from '@/components/FeatherIcon';
+
 export default {
   name: 'home',
   data() {
     return {
       msg: 'Vuepapers',
     };
+  },
+  components: {
+    fIcon,
+  },
+  computed: {
+    ...mapState({
+      allWallpapers: state => state.Contentful.allWallpapers,
+    }),
   },
 };
 </script>
@@ -53,6 +88,53 @@ export default {
 
 .hero,
 .section {
-  min-height: 90vh;
+  min-height: 70vh;
+}
+
+.cards {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  align-content: center;
+  background: $blue;
+  padding-top: 32px;
+  padding-bottom: 32px;
+}
+
+.card {
+  /*flex-grow: 1;*/
+  /*flex-basis: 30%;*/
+  margin: 16px;
+  background: $purple-dark;
+  border: 3px solid $purple;
+  color: $purple;
+  text-decoration: none;
+
+  img {
+    display: block;
+    width: 100%;
+    height: auto;
+  }
+
+  .btn {
+    background: $purple-dark;
+    color: $green;
+    border: none;
+  }
+
+  code {
+    text-align: left;
+    font-size: 10px;
+  }
+
+  pre {
+    max-height: 30em;
+    /*overflow: auto;*/
+  }
+}
+
+.card--controls {
+  margin-bottom: 1em;
 }
 </style>
